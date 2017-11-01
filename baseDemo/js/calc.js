b3b8d7286f4da99492264f7301b977e943e79d7c
maxdosme <233705835@qq.com>
2017-11-01 14:58:04 +0800

    基本打包实例

---
+++ baseDemo/js/calc.js
@@ -0,0 +1,11 @@
+/**
+ * Created by BenTfu on 2017-11-01.
+ */
+
+function add(x,y) {
+    return x + y;
+}
+
+module.exports = {
+    addFun : add
+}
---
+++ baseDemo/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by BenTfu on 2017-11-01.
+ */
+
+var calcExports = require('./calc.js');
+
+console.log(calcExports.addFun(1,2));
